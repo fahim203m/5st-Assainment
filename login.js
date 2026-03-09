9f0db0fd0b7d4df062a9b36adff29ab7723e1fd9
fahim203m <ahmmedshihab205@gnil.com>
2026-03-09 23:04:51 +0600

    6th commit

--- login.js
+++ login.js
@@ -4,18 +4,7 @@ document
 
 e.preventDefault()
 
-const username =
-document.getElementById("username").value
-
-const password =
-document.getElementById("password").value
-
-if(username === "admin" && password === "admin123"){
-
-localStorage.setItem("login", true)
-
-window.location.href = "dashboard.html"
-
+ 
 }else{
 
 alert("Invalid credentials")
